3c7cf21b8aec0f8731fad89511056a37f4855584
JP Bader <jp@zavteq.com>
2021-03-08 20:35:00 -0600

    added controllers

--- lib/hello_web/controllers/hello_controller.ex
+++ lib/hello_web/controllers/hello_controller.ex
@@ -1,6 +1,8 @@
 defmodule HelloWeb.HelloController do
   use HelloWeb, :controller
 
+  action_fallback HelloWeb.MyFallbackController
+
   plug HelloWeb.Plugs.Locale, "en" when action in [:index]
   # plug :authenticate
   # plug :fetch_message
@@ -11,11 +13,31 @@ defmodule HelloWeb.HelloController do
   end
 
   def show(conn, %{"messenger" => messenger}) do
+    conn
+    |> assign(:messenger, messenger)
+    |> assign(:receiver, "blabittyblah")
+    |> render("show.html")
     # render(conn, "show.html", messenger: messenger)
     # text(conn, "From messenger #{messenger}")
-    json(conn, %{id: messenger})
+    # json(conn, %{id: messenger})
+    # html(conn, """
+    #  <html>
+    #    <head>
+    #       <title>Passing a Messenger</title>
+    #    </head>
+    #    <body>
+    #      <p>From messenger #{Plug.HTML.html_escape(messenger)}</p>
+    #    </body>
+    #  </html>
+    # """)
   end
 
+  # from Phoenix 1.5.8 guide on Controllers 
+  # def show(conn, %{"id" => id}, current_user) do
+  #   with {:ok, post} <- fetch_post(id),
+  #     :ok <- authorize_user(current_user, :view, post) do render(conn, "show.json", post: post)
+  #   end
+  # end
 
   # def show(conn, params) do
   #   render(conn, :show, page: conn.assigns[:message])
--- lib/hello_web/controllers/page_controller.ex
+++ lib/hello_web/controllers/page_controller.ex
@@ -2,6 +2,22 @@ defmodule HelloWeb.PageController do
   use HelloWeb, :controller
 
   def index(conn, _params) do
+    # redirect(conn, to: Routes.page_path(conn, :redirect_test))
+    # redirect(conn, external: "https://www.google.com")
+    # redirect(conn, to: "/redirect_test")
+    conn
+    |> put_flash(:info, "Welcome to lordB8r's playground, from flash info!")
+    |> put_flash(:error, "Let's pretend we have an error.")
+    |> render("index.html")
+    # |> put_layout("admin.html")             # render the normal stuff
+    # |> render(:index)
+    # |> put_resp_content_type("text/plain")  # intentionally render an error
+    # |> send_resp(201, "")
+    # |> put_status(202)
+    # |> render("index.html")
+  end
+
+  def redirect_test(conn, _params) do
     render(conn, "index.html")
   end
 end
